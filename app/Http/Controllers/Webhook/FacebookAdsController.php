18265b9e8c62abfbe4f52f217441502c84730b35
Shamsiddin13 <shamsiddinumarov0013@gmail.com>
2025-06-12 01:19:25 +0500

    fixed

--- app/Http/Controllers/Webhook/FacebookAdsController.php
+++ app/Http/Controllers/Webhook/FacebookAdsController.php
@@ -15,14 +15,27 @@ class FacebookAdsController extends Controller
             // Get the entire request payload
             $payload = $request->all();
 
-            $verifyToken = 'oNUfKo0VRzsPWa9SnhHjvWnLadJ6NVrV'; // same as in Meta UI
+            $verifyToken = ['oNUfKo0VRzsPWa9SnhHjvWnLadJ6NVrV', 'EAAKM6WFGk80BO7HBe6bITxPwiclgsLJyZCj4kwyTHUsH7IQpT1TUKAzcGhex25etZARbdRxxTC4jiPv97f3411M05c3mC04TbUx1ZCZAZA5YPScTC4z63WNGRzKReNJiZC9RwLYVN6pL6RlQGUm8na58MopwWzTR3EJlF2ZCamPRAHKGnyWTwVxbMxCtiCZCyiLxj9cfvjCO6JNp30GAeuZBtIOwq']; // same as in Meta UI
 
-            if ($request->input('hub_mode') === 'subscribe' &&  $request->input('hub_verify_token') === $verifyToken) {
+            // Check for Facebook verification request
+            if ($request->has('hub_mode') && $request->input('hub_mode') === 'subscribe' 
+                && $request->has('hub_verify_token') && in_array($request->input('hub_verify_token'), $verifyToken)) {
                 return response($request->input('hub_challenge'), 200)
                     ->header('Content-Type', 'text/plain');
             }
 
-            if ($request->input('hub_mode') !== 'subscribe' && $request->input('hub_verify_token') !== $verifyToken) return response(['Error' => 'Verification failed', 'response' => $request->all()], 403);
+            // Check for Facebook verification request (using hub.mode format)
+            if ($request->has('hub.mode') && $request->input('hub.mode') === 'subscribe' 
+                && $request->has('hub.verify_token') && in_array($request->input('hub.verify_token'), $verifyToken)) {
+                return response($request->input('hub.challenge'), 200)
+                    ->header('Content-Type', 'text/plain');
+            }
+
+            // If verification fails
+            if (($request->has('hub_mode') || $request->has('hub.mode')) && 
+                (!in_array($request->input('hub_verify_token'), $verifyToken) && !in_array($request->input('hub.verify_token'), $verifyToken))) {
+                return response(['Error' => 'Verification failed', 'response' => $request->all()], 403);
+            }
 
             // Create a new UserFacebookLead record
             $lead = new UserFacebookLead();
